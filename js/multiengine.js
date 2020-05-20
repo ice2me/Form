6aff3f9760960d3622f9108cdfaa05d07520a309
Serhii Zuiev <ice2me1989@gmail.com>
2020-05-20 10:40:44 +0300

    1

---
+++ js/multiengine.js
@@ -0,0 +1,37 @@
+// Как только страничка загрузилась 
+window.onload = function() {
+    // проверяем поддерживает ли браузер FormData 
+    if (!window.FormData) {
+        alert("Браузер не поддерживает загрузку файлов на этом сайте");
+    }
+}
+var url = "http://www.dr05045.ho.ua/escort/";
+
+jQuery(document).ready(function() {
+    // =validation
+    var errorTxt = 'Ошибка отправки';
+    jQuery("#sendform").validate({
+        submitHandler: function(form) {
+            var form = document.forms.sendform,
+                formData = new FormData(form),
+                xhr = new XMLHttpRequest();
+
+            xhr.open("POST", "multisend.php");
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        // jQuery("#sendform").html('<p class="thank">Данные отправлены!<p>');
+                        $(location).attr('href', url);
+                    }
+                }
+            };
+            xhr.send(formData);
+        }
+    });
+})
+
+function sendSuccess(callback) {
+    jQuery(callback).find("form fieldset").html(thank);
+    startClock();
+}
